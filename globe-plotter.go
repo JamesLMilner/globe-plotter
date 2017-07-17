22d3d8c3289cc8875a740c938fea08e2287c903a
James Milner <jamesmilner22@hotmail.com>
2017-07-17 23:50:36 +0100

    More input options

--- globe-plotter.go
+++ globe-plotter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"image/color"
 	"io"
@@ -8,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/mmcloughlin/globe"
 	geojson "github.com/paulmach/go.geojson"
@@ -15,10 +17,10 @@ import (
 
 func main() {
 
-	http.Handle("/", http.FileServer(http.Dir("./static/")))
+	fs := http.FileServer(http.Dir("./static/"))
+	http.Handle("/", fs)
 	http.HandleFunc("/upload", uploadHandler)
 	http.ListenAndServe(":8080", nil)
-
 }
 
 var templates = template.Must(template.ParseFiles("static/globe.html"))
@@ -27,13 +29,7 @@ func display(w http.ResponseWriter, tmpl string, data interface{}) {
 	templates.ExecuteTemplate(w, tmpl+".html", data)
 }
 
-func createImage(filename string, uploadUrl string) {
-
-	// url := "./static/example/earthquake.geojson"
-	// data, err := LoadFeatureCollection(url)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+func createImage(filename string, uploadUrl string, rgbaColors rgba, longitude float64, latitude float64) {
 
 	geojson, err := LoadFeatureCollection(uploadUrl)
 	if err != nil {
@@ -44,21 +40,42 @@ func createImage(filename string, uploadUrl string) {
 	g.DrawGraticule(20.0)
 	g.DrawLandBoundaries()
 	//g.DrawCountryBoundaries()
-	green := color.RGBA{100, 254, 0, 100}
+	color := color.RGBA{rgbaColors.R, rgbaColors.G, rgbaColors.B, rgbaColors.A}
+
 	for _, geometries := range geojson.Features {
 		if geometries.Geometry.IsPoint() {
-			log.Println("Point")
+			//log.Println("Point")
 			coords := geometries.Geometry.Point
 			// Lat, Lng, something, color
-			g.DrawDot(coords[0], coords[1], 0.01, globe.Color(green))
+			g.DrawDot(coords[0], coords[1], 0.02, globe.Color(color))
 		}
 	}
-	g.CenterOn(40.645423, -73.903879)
+
+	g.CenterOn(latitude, longitude)
 
 	err = g.SavePNG("./static/generated/"+filename+".png", 800)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type rgba struct {
+	R uint8 `json:"r"`
+	G uint8 `json:"g"`
+	B uint8 `json:"b"`
+	A uint8 `json:"a"`
+}
+
+func getRgbaColor(rgbaStr string) rgba {
+
+	in := []byte(rgbaStr)
+	var raw rgba
+	err := json.Unmarshal(in, &raw)
+	if err != nil {
+		log.Println(err)
+	}
+	return raw
+
 }
 
 //This is where the action happens.
@@ -78,6 +95,23 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		uuid := r.FormValue("uuid")
+		rgbaValue := r.FormValue("rgba")
+		rgbaColors := getRgbaColor(rgbaValue)
+
+		latitude, err := strconv.ParseFloat(r.FormValue("latitude"), 64)
+		if err != nil {
+			latitude = 0.0
+		}
+		longitude, err := strconv.ParseFloat(r.FormValue("longitude"), 64)
+		if err != nil {
+			longitude = 0.0
+		}
+
+		log.Println(rgbaValue)
+		log.Println(rgbaColors)
+		log.Println(latitude)
+		log.Println(longitude)
+
 		var uploadUrl string
 		//get a ref to the parsed multipart form
 		m := r.MultipartForm
@@ -93,7 +127,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 			//create destination file making sure the path is writeable.
-			uploadUrl = "./uploads/" + files[i].Filename
+			uploadUrl = "./upload/" + files[i].Filename
 			dst, err := os.Create(uploadUrl)
 			defer dst.Close()
 			if err != nil {
@@ -108,7 +142,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 		}
 
-		createImage(uuid, uploadUrl)
+		createImage(uuid, uploadUrl, rgbaColors, longitude, latitude)
 		//display success message.
 		//display(w, "upload", "Upload successful.")
 	default:
--- static/js/globe.js
+++ static/js/globe.js
@@ -1,16 +1,60 @@
 (function(){
+    var uuid;
 
+    UUIDChanged();
     initSpectrum();
-    setUUID();
     setRgba();
+
     var form = '.form-signin';
+    var files;
+
+    $("input[name=latitude], input[name=longitude], input[name=rgba]").change(function(){
+        UUIDChanged();
+    })
+
+    $(form).change(function(event){
+
+        if (event.target.files) {
+            var files = event.target.files; // FileList object
+            if (files && files.length > 0) {
+                // use the 1st file from the list
+                var file = files[0];
+                if (file) {
+
+                    UUIDChanged();
+                    var reader = new FileReader();
+
+                    // Closure to capture the file information.
+                    reader.onload = (function(json) {
+                        return function(loadEvent) {
+                            var centroidGeoJson = JSON.parse(loadEvent.target.result);
+                            var centroid = turf.centroid(centroidGeoJson);
+                            var coords = centroid.geometry.coordinates;
+                            setLatitude(coords[1].toFixed(5));
+                            setLongitude(coords[0].toFixed(5));
+                            console.log(coords);
+                        }
+                    
+                    })(file);
+
+                    // Read in the image file as a data URL.
+                    reader.readAsText(file);
+
+                }
+            }
+        }
+
+    });
     
     $(form).submit(function (event) {
         var data = new FormData($(form)[0]);
         rgba = $("#colorpicker").spectrum("get").toRgb();
+        
         setRgba(rgba);
-        console.log(data);
 
+        console.log("latitude", $("input[name=latitude]").val());
+        console.log("longitude", $("input[name=longitude]").val());
+     
         event.preventDefault();
         $.ajax({
             url:'/upload',
@@ -22,26 +66,48 @@
                 $('.status').text("Upload sent!");
                 $('.output').empty();
                 var img = '<img src="/generated/' + uuid + '.png">';
+                console.log(img);
                 $('.output').html(img);
             }
         });
         return false;
     });
 
+    function onColorChange() {
+        setRgba();
+        UUIDChanged();
+    }
+
     function initSpectrum() {
         $("#colorpicker").spectrum({
-            color: "rgb(240, 110, 110)"
+            showAlpha: true,
+            color: "rgba(240, 110, 110, 0.95)",
+            change: onColorChange
         });
     }
 
     function setRgba() {
         var rgba = $("#colorpicker").spectrum("get").toRgb();
+        rgba = JSON.stringify(rgba);
+        console.log(rgba);
         $("input[type=hidden][name=rgba]").val(rgba);
     }
 
-    function setUUID() {
-        var uuid = uuidv4();
-        $("input[type=hidden][name=uuid]").val(uuid);
+    function setLatitude(latitude) {
+        $("input[name=latitude]").val(latitude);
+    }
+
+    function setLongitude(longitude) {
+        $("input[name=longitude]").val(longitude);
+    }
+
+    function setUUID(UUID) {
+        $("input[type=hidden][name=uuid]").val(UUID);
+    }
+
+    function UUIDChanged() {
+        uuid = uuidv4();
+        setUUID(uuid);
     }
 
 
